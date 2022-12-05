c51d080c06e85e5bead243dfd9c4fc0b848b5c28
vlatrof <gthbttr@gmail.com>
2022-12-05 20:03:30 +0300

    add column

--- app/src/main/java/com/vlatrof/jetcomdemo/MainActivity.kt
+++ app/src/main/java/com/vlatrof/jetcomdemo/MainActivity.kt
@@ -3,57 +3,62 @@ package com.vlatrof.jetcomdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.vlatrof.jetcomdemo.ui.theme.JetComDemoTheme
-import com.vlatrof.jetcomdemo.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetComDemoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Column {
-                        Greeting("Android")
-                        CustomText(text = "Jetpack Compose")
-                    }
-                }
-            }
+            MainActivityScreen()
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Composable
-fun CustomText(text: String) {
-    Text(
-        text = text,
-        style = Typography.h5
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    JetComDemoTheme {
-        Column {
-            Greeting(name = "Android")
-            CustomText(text = "Jetpack Compose")
+    MainActivityScreen()
+}
+
+@Composable
+fun MainActivityScreen() {
+    JetComDemoTheme() {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            CustomItem(
+                weight = 1f,
+                color = MaterialTheme.colors.primary
+            )
+            CustomItem(
+                weight = 2f,
+                color = MaterialTheme.colors.primaryVariant
+            )
         }
     }
+}
+
+@Composable
+fun ColumnScope.CustomItem(weight: Float, color: Color = MaterialTheme.colors.primary) {
+    Surface(
+        modifier = Modifier
+            .width(200.dp)
+            .weight(weight),
+        color = color
+    ) {}
 }
